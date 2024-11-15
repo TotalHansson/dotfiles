c16f2c6170c4c1392899111e1490d5d10b81451f
Markus Hansson <markus273@hotmail.com>
2024-11-15 13:40:53 +0100

    Add markdown preview command to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,6 +132,17 @@ require('lazy').setup({
     },
   },
 
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
+
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
   {
@@ -759,6 +770,42 @@ require('onedark').setup {
 }
 require('onedark').load()
 vim.api.nvim_set_hl(0, 'Folded', { fg = 'Gray' })
+
+require('peek').setup({
+  auto_load = true,         -- whether to automatically load preview when
+                            -- entering another markdown buffer
+  close_on_bdelete = true,  -- close preview window on buffer delete
+
+  syntax = true,            -- enable syntax highlighting, affects performance
+
+  theme = 'dark',           -- 'dark' or 'light'
+
+  update_on_change = true,
+
+  app = 'webview',          -- 'webview', 'browser', string or a table of strings
+                            -- explained below
+
+  filetype = { 'markdown' },-- list of filetypes to recognize as markdown
+
+  -- relevant if update_on_change is true
+  throttle_at = 200000,     -- start throttling when file exceeds this
+                            -- amount of bytes in size
+  throttle_time = 'auto',   -- minimum amount of time in milliseconds
+                            -- that has to pass before starting new render
+})
+local peek = require('peek')
+vim.api.nvim_create_user_command('MarkdownPreviewOpen', function ()
+  if not peek.is_open() and vim.bo[vim.api.nvim_get_current_buf()].filetype == 'markdown' then
+    vim.fn.system('i3-msg split horizontal')
+    peek.open()
+  end
+end, {})
+vim.api.nvim_create_user_command('MarkdownPreviewClose', function ()
+  if peek.is_open() then
+    peek.close()
+    vim.fn.system('i3-msg move left')
+  end
+end, {})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
