86d8732c2d7b7d79387c1defd804dc87a498d08a
Markus Hansson <markus273@hotmail.com>
2025-09-29 14:35:38 +0200

    Add live_grep_with_args to nvim
    When using leader-s-g to search with grep, pressing ctrl+space will
    start a new grep search on the current results only

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -272,7 +272,20 @@ require('lazy').setup({
           return vim.fn.executable 'make' == 1
         end,
       },
+      {
+        'nvim-telescope/telescope-live-grep-args.nvim' ,
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = '^1.0.0',
+      },
     },
+    config = function()
+      local telescope = require('telescope')
+
+      -- telescope.setup({})
+
+      telescope.load_extension('live_grep_args')
+    end
   },
 
   {
