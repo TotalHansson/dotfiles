1f88d346995582d63d14062f2c099c93fbe9964e
Markus Hansson <markus-hansson@skf.com>
2026-02-25 15:48:05 +0100

    Add ensure_installed tools to Mason

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -691,9 +691,14 @@ require("lazy").setup({
 			-- for you, so that they are available from within Neovim.
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
 				"gofumpt", -- Used to format Go code
 				"goimports", -- Used to format Go code
+				"gopls",
+				"helm-ls",
+				"hyprls",
+				"lua-language-server",
+				"stylua", -- Used to format Lua code
+				"yaml-language-server",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
